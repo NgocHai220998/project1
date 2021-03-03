716aaca6f04ba35b0008774e68f5eb36ec8be968
nga20167304 <hocmetmuonchet@gmail.com>
2021-03-03 12:18:18 +0700

    fix rspec

--- spec/factories/spot.rb
+++ spec/factories/spot.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :spot do
     prefecture
-    name {"原生花園 あやめヶ原"}
-    body {"厚岸海岸チンベの鼻一帯の台地上に，野性のヒオウギアヤメ厚岸海岸チンベの鼻一帯の台地上に，野性のヒオウギアヤメ"}
+    sequence(:name) { |n| "#{n}原生花園" }
+    sequence(:body) { |n| "#{n}厚岸海岸チンベの鼻一帯の台地上に，野性のヒオウギアヤメ厚岸海岸チンベの鼻一帯の台地上に，野性のヒオウギアヤメ"}
   end
 end
--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -3,8 +3,7 @@ include Capybara::RSpecMatchers
 
 RSpec.describe "Spots", type: :request do
   describe "GET /spots" do 
-    let(:spot_tag) {FactoryBot.create :spot_tag}
-    before { FactoryBot.create_list(:spot_tag, 31) }
+    let!(:spot_tags){ FactoryBot.create_list(:spot_tag, 31) }
     before(:each) do
       get root_path
     end
@@ -14,23 +13,21 @@ RSpec.describe "Spots", type: :request do
     end
 
     it "tag_nameが表示されていること" do
-      expect(spot_tag.tag.name).to eq("高原")
-      expect(response.body).to include("高原")
+      expect(response.body).to include(spot_tags[0].tag.name)
     end
 
     it "prefectureの名前が表示されていること" do
-      expect(spot_tag.spot.prefecture.name).to eq("北海道")
-      expect(response.body).to include("北海道")
+      expect(response.body).to include(spot_tags[0].spot.prefecture.name)
     end
 
     it "spotの名前が表示されていること" do
-      expect(spot_tag.spot.name).to eq("原生花園 あやめヶ原")
-      expect(response.body).to include("原生花園 あやめヶ原")
+      expect(response.body).to include(spot_tags[0].spot.name)
     end
 
     it "spotのbodyが30字以内で表示されていること" do
-      expect(spot_tag.spot.body.truncate(30).length).to eq(30)
-      expect(response.body).to include("厚岸海岸チンベの鼻一帯の台地上に")
+      expect(spot_tags[0].spot.body.truncate(30).length).to eq(30)
+      expect(response.body).to have_content(spot_tags[0].spot.body.truncate(30))
+      byebug
     end
 
     it "ページネーションが表示されていること" do
@@ -39,7 +36,7 @@ RSpec.describe "Spots", type: :request do
 
     it "ページ2を表示されていること" do
       get "/?page=2"
-      expect(response.body).to include("北海道")
+      expect(response.body).to include(spot_tags[30].spot.name)
     end
   end
 end
