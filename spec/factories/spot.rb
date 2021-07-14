be7b891fd5463ec2a15e9ba6647f15ace1f58215
nga20167304 <hocmetmuonchet@gmail.com>
2021-07-14 07:58:07 +0000

    kadai6

--- spec/factories/spot.rb
+++ spec/factories/spot.rb
@@ -5,20 +5,16 @@ FactoryBot.define do
     sequence(:name) { |n| "#{n}原生花園" }
     sequence(:body) { |n| "#{n}厚岸海岸チンベの鼻一帯の台地上に，野性のヒオウギアヤメ厚岸海岸チンベの鼻一帯の台地上に，野性のヒオウギアヤメ"}
     
-    trait :with_spot_review do
-      after(:create) do |spot|
-        FactoryBot.create_list(:spot_review, rand(1..50), spot: spot)
-      end
-    end
-
-    trait :with_spot_schedule do
+    trait :with_spot_review_and_spot_schedule do
       transient do
+        reviews_count {5}
         start_on { Time.zone.now - rand(11..30).days }
         end_on { Time.zone.now - rand(1..10).days }
       end
-      
+
       after(:build) do |spot, evaluator|
-        spot.spot_schedules << FactoryBot.create(:spot_schedule, start_on: evaluator.start_on, end_on: evaluator.end_on)
+        spot.spot_reviews << FactoryBot.build_list(:spot_review, evaluator.reviews_count)
+        spot.spot_schedules << FactoryBot.build(:spot_schedule, start_on: evaluator.start_on, end_on: evaluator.end_on)
       end
     end
   end
