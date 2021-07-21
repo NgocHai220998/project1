57ddcf12ad310952f085f11f547f57df58de3ac7
nga20167304 <hocmetmuonchet@gmail.com>
2021-07-21 10:21:56 +0700

    fix indent

--- app/javascript/packs/spots/spots.js
+++ app/javascript/packs/spots/spots.js
@@ -1,47 +1,47 @@
 document.addEventListener("turbolinks:load",function() {
   x = document.getElementsByClassName("review-modal");
 
-  for(var i = 0; i<x.length; i++) {
+  for(let i = 0; i<x.length; i++) {
     x[i].onclick = function () {
-      const recentReview = document.getElementById(`spot-${this.dataset.id}`)
-      recentReview.classList.toggle("hidden")
+      const recentReview = document.getElementById(`spot-${this.dataset.id}`);
+      recentReview.classList.toggle('hidden');
     };
   }
 
-  var tag_select = document.querySelector('#q_tag_name_in');
+  const tagSelect = document.querySelector('#q_tag_name_in');
 
-  let selectedValues = []
+  let selectedValues = [];
 
   function handleSelect(selectedValue){
     let elSelectBox = document.querySelector('.multi-select');
-    let selectedHTML = "";
+    let selectedHTML = '';
 
     if (selectedValue && selectedValue.length > 2) {
-      document.querySelector(".number-of-count").classList.remove("hidden")
-      document.querySelector(".number-of-count span").innerHTML = `... +${selectedValue.length - 2}`
+      document.querySelector('.number-of-count').classList.remove('hidden');
+      document.querySelector('.number-of-count span').innerHTML = `... +${selectedValue.length - 2}`;
     } else {
-      document.querySelector(".number-of-count").classList.add("hidden")
+      document.querySelector('.number-of-count').classList.add('hidden');
     }
     if (!selectedValues || (selectedValue && selectedValue.length === 0)) {
-      elSelectBox.innerHTML = `<div class="select-item-placeholder">設定なし</div>`
+      elSelectBox.innerHTML = `<div class="select-item-placeholder">設定なし</div>`;
     } else {
       for(let i=0; i<selectedValue.length; i++) {
-        selectedHTML = selectedHTML + `<div class="select-item">${selectedValue[i]}</div>`
+        selectedHTML = selectedHTML + `<div class="select-item">${selectedValue[i]}</div>`;
       }
       elSelectBox.innerHTML = selectedHTML;
     }
   }
 
-  var elSelected = document.querySelectorAll('#q_tag_name_in option[selected=selected]')
+  const elSelected = document.querySelectorAll('#q_tag_name_in option[selected=selected]');
 
   if (elSelected && elSelected.length > 0) {
     for (let i = 0; i < elSelected.length; ++i) {
-      selectedValues.push(elSelected[i].innerText)
+      selectedValues.push(elSelected[i].innerText);
     }
     handleSelect(selectedValues);
   }
 
-  tag_select.addEventListener('change', (e) => { 
+  tagSelect.addEventListener('change', (e) => { 
     const options = e.target.options;
     const newSelectedValues = [];
 
@@ -59,28 +59,26 @@ document.addEventListener("turbolinks:load",function() {
     handleSelect(selectedValues);
   })
 
-  var options = Array.from(document.querySelectorAll("#q_tag_name_in option"));
+  const options = Array.from(document.querySelectorAll('#q_tag_name_in option'));
 
-  options.forEach(function (element)
-  {
-    element.addEventListener("mousedown", 
-        function (e)
-        {
-           if (e.shiftKey) return;
-    e.preventDefault();
-    tag_select.focus();
-    var scroll = tag_select.scrollTop;
-    e.target.selected = !e.target.selected;
-     setTimeout(function(){tag_select.scrollTop = scroll;}, 0);
-    tag_select.dispatchEvent(new Event("change"));
-        }
-        , false
+  options.forEach(function (element) {
+    element.addEventListener('mousedown', 
+      function (e) {
+        if (e.shiftKey) return;
+        e.preventDefault();
+        tagSelect.focus();
+        let scroll = tagSelect.scrollTop;
+        e.target.selected = !e.target.selected;
+        setTimeout(function(){tagSelect.scrollTop = scroll;}, 0);
+        tagSelect.dispatchEvent(new Event('change'));
+      }
+    , false
     );
   });
 
-  var multi_select = document.querySelector(".multi-select");
+  const multiSelect = document.querySelector('.multi-select');
 
-  multi_select.onclick = function (){
-    document.querySelector("#q_tag_name_in").classList.toggle("hidden");
+  multiSelect.onclick = function (){
+    document.querySelector('#q_tag_name_in').classList.toggle('hidden');
   };
 });
