846f70964dfa264bf666e3bb2164b5a662c63143
nga20167304 <hocmetmuonchet@gmail.com>
2021-08-20 10:42:45 +0700

    decorator test

--- spec/decorators/spot_decorator_spec.rb
+++ spec/decorators/spot_decorator_spec.rb
@@ -3,35 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe SpotDecorator do
-  describe 'restroom_qty_status' do
-    it 'restroom_qtyがnil場合' do
-      spot = Spot.new
-      decorated_spot = ActiveDecorator::Decorator.instance.decorate(spot)
-
-      expect(decorated_spot.restroom_qty_status).to eq(0)
+  describe 'restroom_qtyとwifiがnil' do
+    let!(:spot1) do
+      Spot.new
     end
 
-    it 'restroom_qtyがnilじゃ無い場合' do
-      spot = Spot.new(restroom_qty: "5")
-      decorated_spot = ActiveDecorator::Decorator.instance.decorate(spot)
+    subject do
+      decorated_spot = ActiveDecorator::Decorator.instance.decorate(spot1)
+    end
 
-      expect(decorated_spot.restroom_qty_status).to eq("5")
+    it '正しい結果' do
+      subject
+      expect(subject.restroom_qty_status).to eq(0)
+      expect(subject.wifi_status).to eq('無')
     end
   end
 
-  describe 'wifi_status' do
-    it 'wifiがnil場合' do
-      spot = Spot.new
-      decorated_spot = ActiveDecorator::Decorator.instance.decorate(spot)
-
-      expect(decorated_spot.wifi_status).to eq('無')
+  describe 'restroom_qtyとwifiがnilじゃない' do
+    let!(:spot1) do
+      Spot.new(restroom_qty: 5, wifi: '有')
     end
 
-    it 'restroom_qtyがnilじゃ無い場合' do
-      spot = Spot.new(wifi: '有')
-      decorated_spot = ActiveDecorator::Decorator.instance.decorate(spot)
+    subject do
+      decorated_spot = ActiveDecorator::Decorator.instance.decorate(spot1)
+    end
 
-      expect(decorated_spot.wifi_status).to eq('有')
+    it '正しい結果' do
+      expect(subject.restroom_qty_status).to eq('5')
+      expect(subject.wifi_status).to eq('有')
     end
   end
 end
