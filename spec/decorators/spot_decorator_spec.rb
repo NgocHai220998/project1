2ca2454dfe3ff2616b45fc302256a1cbcabb237a
nga20167304 <hocmetmuonchet@gmail.com>
2021-08-20 11:22:48 +0700

    decorator test

--- spec/decorators/spot_decorator_spec.rb
+++ spec/decorators/spot_decorator_spec.rb
@@ -3,13 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe SpotDecorator do
+  subject do
+    ActiveDecorator::Decorator.instance.decorate(spot1)
+  end
+
   describe 'restroom_qtyとwifiがnil' do
     let!(:spot1) do
       Spot.new
-    end
-
-    subject do
-      decorated_spot = ActiveDecorator::Decorator.instance.decorate(spot1)
     end
 
     it '正しい結果' do
@@ -22,10 +22,6 @@ RSpec.describe SpotDecorator do
   describe 'restroom_qtyとwifiがnilじゃない' do
     let!(:spot1) do
       Spot.new(restroom_qty: 5, wifi: '有')
-    end
-
-    subject do
-      decorated_spot = ActiveDecorator::Decorator.instance.decorate(spot1)
     end
 
     it '正しい結果' do
