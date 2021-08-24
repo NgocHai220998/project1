21ab35bc3ecb47843e8dceaec9f5501caccc2a54
nga20167304 <hocmetmuonchet@gmail.com>
2021-08-24 17:28:08 +0700

    change validation spot_review_count to model

--- app/controllers/spot_reviews_controller.rb
+++ app/controllers/spot_reviews_controller.rb
@@ -6,7 +6,7 @@ class SpotReviewsController < ApplicationController
   def create
     if logged_in?
       @spot_review = SpotReview.new(post_params)
-      check_spot_review_max
+      handle_save
     else
       flash[:dager] = 'ログイン必要'
       redirect_to login_url
@@ -15,22 +15,19 @@ class SpotReviewsController < ApplicationController
 
   private
 
-  def count_of_spot_review
-    @spot = Spot.find_by(id: post_params[:spot_id])
-
-    @spot.spot_reviews.select { |spot_review| spot_review.user_id == @current_user.id }.count
-  end
-
-  def check_spot_review_max
-    if count_of_spot_review > 2
-      flash[:danger] = 'それぞれのユーザーがそれぞれのスポートにレービューを書く数が３回だけです。'
-      redirect_to root_path
-    elsif @spot_review.save
-      redirect_to spot_path(post_params[:spot_id])
-    end
-  end
-
   def post_params
     params.permit(:comment, :user_id, :spot_id)
+  end
+
+  def handle_save
+    check = SpotReview.check_spot_review_max(post_params[:spot_id], @current_user.id)
+
+    if check
+      flash[:danger] = 'それぞれのユーザーがそれぞれのスポートにレービューを書く数が３回だけです。'
+      redirect_to root_path
+    else
+      @spot_review.save
+      redirect_to spot_path(post_params[:spot_id])
+    end
   end
 end
--- app/models/spot_review.rb
+++ app/models/spot_review.rb
@@ -1,4 +1,16 @@
 class SpotReview < ApplicationRecord
   belongs_to :spot, counter_cache: true
   belongs_to :user
+
+  class << self
+    def count_of_spot_review(spot_id, user_id)
+      @spot = Spot.find_by(id: spot_id)
+
+      @spot.spot_reviews.select { |spot_review| spot_review.user_id == user_id }.count
+    end
+
+    def check_spot_review_max(spot_id, user_id)
+      count_of_spot_review(spot_id, user_id) > 2
+    end
+  end
 end
