28a1427049d2ba5fcc687a273be68affed55fbde
nga20167304 <hocmetmuonchet@gmail.com>
2021-08-25 15:10:14 +0700

    kadai10

--- app/controllers/spot_reviews_controller.rb
+++ app/controllers/spot_reviews_controller.rb
@@ -1,27 +1,25 @@
 class SpotReviewsController < ApplicationController
+  before_action :logged_in_user, only: %i[create]
+
   def new
     @spot_review = SpotReview.new
   end
 
   def create
-    if logged_in?
-      @spot_review = SpotReview.new(post_params)
-      handle_save
+    @spot_review = SpotReview.new(post_params)
+
+    if @spot_review.save
+      flash[:success] = 'レビューできました'
     else
-      flash[:dager] = 'ログイン必要'
-      redirect_to login_url
+      flash[:danger] = 'ユーザーがこのスポートにレービューを書く数が３回ので、もうレビューを書くことができない'
     end
+
+    redirect_to spot_path(post_params[:spot_id])
   end
 
   private
 
   def post_params
     params.permit(:comment, :user_id, :spot_id)
-  end
-
-  def handle_save
-    flash[:danger] = 'ユーザーがこのスポートにレービューを書く数が３回ので、もうレビューを書くことができない' unless @spot_review.save
-    
-    redirect_to spot_path(post_params[:spot_id])
   end
 end
